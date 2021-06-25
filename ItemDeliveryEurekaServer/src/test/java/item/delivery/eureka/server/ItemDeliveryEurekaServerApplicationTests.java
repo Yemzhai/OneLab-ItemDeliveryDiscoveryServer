6f4ffedde716e671cc9fd465b5943aeb7ee9b62d
islam <yemzhayev.i@codeguru.be>
2021-06-25 17:12:57 +0600

    Discovery server spring cloud(eureka server)

---
+++ ItemDeliveryEurekaServer/src/test/java/item/delivery/eureka/server/ItemDeliveryEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package item.delivery.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemDeliveryEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
